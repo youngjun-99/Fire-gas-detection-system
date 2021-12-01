c82aca3970415e657a1d0839b21200cf67c51af1
YoungJunRo <83204523+YoungJunRo@users.noreply.github.com>
2021-12-01 10:24:48 +0900

    Add files via upload

---
+++ peizo_led_client.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+import RPi.GPIO as GPIO
+import time
+import board
+import neopixel
+
+pixels = neopixel.NeoPixel(board.D18, 30)
+
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+
+driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=options)
+driver.get('http://192.168.137.127')
+        
+while True:
+    time.sleep(10)
+    fire = driver.find_element_by_css_selector('body > span.fire > em').text
+    gas = driver.find_element_by_css_selector('body > span.gas > em').text
+    print(fire)
+    print(gas)
+    ifire = float(fire)
+    igas = float(gas)
+    if ifire == 0 and igas >= 300:
+        buzzer = 23
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(buzzer, GPIO.OUT)
+        GPIO.setwarnings(False)
+        list = [261, 294, 329, 349, 392, 440, 493 ,523]
+        while True:
+            try:
+                p = GPIO.PWM(buzzer, 100) 
+                p.start(100)
+                p.ChangeDutyCycle(90)
+                for i in range(8):
+                    p.ChangeFrequency(list[i])
+                    time.sleep(1)
+                p.stop()
+                pixels.fill((255, 0, 0))
+            except:pass
+    else:pass
+
+
